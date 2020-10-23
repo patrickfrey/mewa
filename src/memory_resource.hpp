fdc775490675b9271d591c2546f817929d59cb3f
Patrick Frey <patrickpfrey@yahoo.com>
2020-10-23 15:26:35 +0200

    switch off always throwing allocator as upstream of monotonic buffer resource in release build

--- src/memory_resource.hpp
+++ src/memory_resource.hpp
@@ -16,12 +16,9 @@
 #include <cstddef>
 #include <new>
 
+#ifndef NDEBUG
 #define MEWA_LOWLEVEL_DEBUG
 //... using bad_alloc_memory_resource as upstream of monotonic buffer resource
-#ifdef NDEBUG
-#ifdef MEWA_LOWLEVEL_DEBUG
-#error Switched on MEWA_LOWLEVEL_DEBUG in release build
-#endif
 #endif
 
 namespace mewa {
