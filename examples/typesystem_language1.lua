8763ff12f96a786a0bb3cc540680cc149b154c24
Patrick Frey <patrickpfrey@yahoo.com>
2020-10-24 19:39:36 +0200

    working on AST tree traversal -- in progress (2)

--- examples/typesystem_language1.lua
+++ examples/typesystem_language1.lua
@@ -52,38 +52,42 @@ function initFirstClassCitizens()
 end
 
 function traverse( node, context)
-	local rt = {}
-	for ii, vv in ipairs( node.arg) do
-		local subnode = node.arg[ ii]
-		if subnode.call then
-			if subnode.call.obj then
-				rt[ ii] = subnode.call.proc( subnode, subnode.call.obj, context)
+	if node.arg then
+		local rt = {}
+		for ii, vv in ipairs( node.arg) do
+			local subnode = node.arg[ ii]
+			if subnode.call then
+				if subnode.call.obj then
+					rt[ ii] = subnode.call.proc( subnode, subnode.call.obj, context)
+				else
+					rt[ ii] = subnode.call.proc( subnode, context)
+				end
 			else
-				rt[ ii] = subnode.call.proc( subnode, context)
+				rt[ ii] = subnode
 			end
-		else
-			rt[ ii] = subnode.arg[ ii]
 		end
+		return rt
+	else
+		return node
 	end
-	return rt
 end
 
-function typesystem.vardef( node) return node.call.name end
-function typesystem.vardef_assign( node) return node.call.name end
-function typesystem.vardef_array( node) return node.call.name end
-function typesystem.vardef_array_assign( node) return node.call.name end
-function typesystem.operator( node, opdescr) return node.call.name end
-function typesystem.stm_expression( node) return node.call.name end
-function typesystem.stm_return( node) return node.call.name end
-function typesystem.conditional_if( node) return node.call.name end
-function typesystem.conditional_while( node) return node.call.name end
+function typesystem.vardef( node) return {name=node.call.name, arg=traverse( node)} end
+function typesystem.vardef_assign( node) return {name=node.call.name, arg=traverse( node)} end
+function typesystem.vardef_array( node) return {name=node.call.name, arg=traverse( node)} end
+function typesystem.vardef_array_assign( node) return {name=node.call.name, arg=traverse( node)} end
+function typesystem.operator( node, opdescr) return {name=node.call.name, arg=traverse( node)} end
+function typesystem.stm_expression( node) return {name=node.call.name, arg=traverse( node)} end
+function typesystem.stm_return( node) return {name=node.call.name, arg=traverse( node)} end
+function typesystem.conditional_if( node) return {name=node.call.name, arg=traverse( node)} end
+function typesystem.conditional_while( node) return {name=node.call.name, arg=traverse( node)} end
 
-function typesystem.namespaceref( node) return node.call.name end
-function typesystem.typedef( node) return node.call.name end
-function typesystem.typespec( node, typedescr) return node.call.name end
-function typesystem.funcdef( node) return node.call.name end
-function typesystem.procdef( node) return node.call.name end
-function typesystem.paramdef( node) return node.call.name end
+function typesystem.namespaceref( node) return {name=node.call.name, arg=traverse( node)} end
+function typesystem.typedef( node) return {name=node.call.name, arg=traverse( node)} end
+function typesystem.typespec( node, typedescr) return {name=node.call.name, arg=traverse( node)} end
+function typesystem.funcdef( node) return {name=node.call.name, arg=traverse( node)} end
+function typesystem.procdef( node) return {name=node.call.name, arg=traverse( node)} end
+function typesystem.paramdef( node) return {name=node.call.name, arg=traverse( node)} end
 
 function typesystem.program( node)
 	initFirstClassCitizens()
