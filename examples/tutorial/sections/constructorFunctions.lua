c0bd2ea0172b6c415285569d5644d762c6fd7704
Patrick Frey <patrickpfrey@yahoo.com>
2021-05-09 08:44:27 +0200

    Working on tutorial -- in progress

--- examples/tutorial/sections/constructorFunctions.lua
+++ examples/tutorial/sections/constructorFunctions.lua
@@ -11,32 +11,33 @@ end
 
 -- Builds the argument string and the argument build-up code for a function call or interface method call constructors
 function buildCallArguments( subst, thisTypeId, thisConstructor, args, types)
-	local this_inp,this_code = constructorParts(thisConstructor or "%UNDEFINED")
+	local this_inp,code = constructorParts(thisConstructor or "%UNDEFINED")
 	local argstr = ""
 	if thisTypeId ~= 0 then argstr = typeDescriptionMap[ thisTypeId].llvmtype .. " " .. this_inp .. ", " end
-	code = code .. this_code
 	for ii=1,#args do
 		local arg = args[ ii]
-		subst[ "arg" .. ii] = arg
+		local arg_inp,arg_code = constructorParts( arg)
+		subst[ "arg" .. ii] = arg_inp
 		if types then
 			local llvmtype = typeDescriptionMap[ types[ ii]].llvmtype
-			local arg_inp,arg_code = constructorParts( arg)
 			code = code .. arg_code
 			if llvmtype then argstr = argstr .. llvmtype .. " " .. tostring(arg_inp) .. ", " else argstr = argstr .. "i32 0, " end
 		end
 	end
-	if types and (#args > 0 or thisTypeId ~= 0) then argstr = argstr:sub(1, -3) end
+	if types and thisTypeId ~= 0 then argstr = argstr:sub(1, -3) end
 	subst.argstr = argstr
+	subst.this = this_inp
 	return code
 end
 -- Constructor of a call with an self argument and some additional arguments
-function callConstructor( fmt, thisTypeId, argTypeIds)
+function callConstructor( fmt, thisTypeId, argTypeIds, vartable)
 	return function( this_constructor, arg_constructors)
 		env = getCallableEnvironment()
 		local out = env.register()
-		local subst = {out=out, this=this_constructor}
+		local subst = utils.deepCopyStruct( vartable) or {}
+		subst.out = out
 		local code = buildCallArguments( subst, thisTypeId or 0, this_constructor, arg_constructors, argTypeIds)
-		code = code .. utils.constructor_format( fmt, {out=out, this=value}, env.register)
+		code = code .. utils.constructor_format( fmt, subst, env.register)
 		return {code=code,out=out}
 	end
 end
