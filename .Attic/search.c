754a018aa7e9623932f182e9df851db684465648
Patrick Frey <patrickpfrey@yahoo.com>
2021-08-25 10:12:55 +0200

    Working on pure C implementaion -- in progress

--- .Attic/search.c
+++ .Attic/search.c
@@ -1,3 +1,8 @@
+#include <inttypes.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 typedef struct SPSStackElement {
 	float weight;
@@ -12,18 +17,21 @@ typedef struct FollowElement {
 
 typedef struct SPSTreeElement {
 	int32_t type;
-	int32_t prev;
 	int32_t constructor;
+	int32_t prev;
 } SPSTreeElement;
 
-#define MAX_SEARCH_SIZE 256
-#define MAX_TREE_SIZE 2048
-
-enum ErrorCode {
+enum Constants {
+	MAX_PATH_SIZE = 32,
+	MAX_SEARCH_SIZE = 256,
+	MAX_TREE_SIZE = 2048
+};
+typedef enum {
 	Ok=0,
 	LogicError=1,
 	BufferTooSmall=2
-};
+} ErrorCode;
+
 static const char* errorString( ErrorCode errcode)
 {
 	static const char* ar[] = {
@@ -33,7 +41,7 @@ static const char* errorString( ErrorCode errcode)
 	};
 	return ar[ errcode];
 }
-static void throwError( ErrorCode errcode, const char* msg=0)
+static void throwError( ErrorCode errcode, const char* msg)
 {
 	if (msg) {
 		fprintf( stderr, "Exception %d: %s (%s)\n", (int)errcode, errorString( errcode), msg);
@@ -43,24 +51,50 @@ static void throwError( ErrorCode errcode, const char* msg=0)
 	exit( 1);
 }
 
-typedef size_t (*GetFollow)( FollowElement* buf, void* context, int32_t type);
+typedef size_t (*SPSGetFollow)( FollowElement* buf, void* context, int32_t type);
+typedef int (*SPSMatch)( void* context, int32_t type);
 
 typedef struct TypeConstructorPair
 {
 	int32_t type;
 	int32_t constructor;
-};
+} TypeConstructorPair;
 
 typedef struct ShortestPathSearchResult
 {
 	float weight;
+	int32_t fromindex;
 	size_t arsize;
-	TypeConstructorPair ar[ MAX_SEARCH_SIZE];
+	TypeConstructorPair ar[ MAX_PATH_SIZE];
 	size_t arsize_alt;
-	TypeConstructorPair ar_alt[ MAX_SEARCH_SIZE];
-};
+	TypeConstructorPair ar_alt[ MAX_PATH_SIZE];
+} ShortestPathSearchResult;
 
-static bool shortestPathSearch( int32_t desttype, int32_t* fromtype, size_t fromtypesize, GetFollow getFollow, void* getFollowContext)
+static size_t fillResult( TypeConstructorPair* ar, SPSTreeElement const* tree, int32_t ti)
+{
+	size_t ai = 0;
+	while (ti >= 0)
+	{
+		if (ai >= MAX_PATH_SIZE) throwError( BufferTooSmall, 0);
+		ar[ ai].type = tree[ti].type;
+		ar[ ai].constructor = tree[ti].constructor;
+		ti = tree[ti].prev;
+	}
+	size_t bn = (ti & ~1) /2;
+	size_t bi = 0;
+	for (; bi <= bn; ++bi)
+	{
+		// swap
+		int32_t tt = ar[bi].type;
+		int32_t cc = ar[bi].constructor;
+		ar[bi].type = ar[bn].type;
+		ar[bi].constructor = ar[bn].constructor;
+		ar[bn].type = tt;
+		ar[bn].constructor = cc;
+	}
+	return ai;
+}
+static int shortestPathSearch( ShortestPathSearchResult* result, SPSMatch match, int32_t* fromtype, size_t fromtypesize, SPSGetFollow getFollow, void* context)
 {
 	SPSTreeElement tree[ MAX_TREE_SIZE];
 	SPSStackElement stk[ MAX_SEARCH_SIZE];
@@ -68,32 +102,42 @@ static bool shortestPathSearch( int32_t desttype, int32_t* fromtype, size_t from
 	size_t followsize = 0;
 	size_t stksize = 0;
 	size_t treesize = 0;
-	bool stksel = false;
 	size_t fi = 0;
+	result->weight = 0.0;
+	result->fromindex = -1;
+	result->arsize = 0;
+	result->arsize_alt = 0;
 	memset( &stk, 0, sizeof(stk));
 	for (; fi != fromtypesize; ++fi)
 	{
-		SPSTreeElement* te = &stk[ fi];
-		{te->type = fromtype[ fi]; te->weight = 0.0; te->prev = -1;}
+		if (match( context, fromtype[ fi]))
+		{
+			result->fromindex = fi;
+			return 1;
+		}
+		SPSTreeElement* te = &tree[ fi];
+		{te->type = fromtype[ fi]; te->constructor = fi; te->prev = -1;}
+		stk[ fi].idx = fi;
+		stk[ fi].weight = 0.0;
 	}
 	stksize = fromtypesize;
 	while (stksize > 0)
 	{
 		SPSStackElement* elem = &stk[ stksize-1];
 		int32_t fotreeidx = elem->idx;
-		followsize = getFollow( follow, getFollowContext, elem->type);
+		followsize = getFollow( follow, context, tree[ fotreeidx].type);
 		for (fi=0; fi<followsize; ++fi)
 		{
 			int32_t fotype = follow[ fi].type;
 			float foweight = elem->weight + follow[ fi].weight;
 			size_t si = stksize;
 			for (; si > 0 && stk[ si-1].weight < foweight; --si){}
-			size_t ti = elem->idx;
-			for (; tree[ti].type != fotype && tree[ti].idx >= 0; ti = tree[ti].idx){}
+			size_t ti = fotreeidx;
+			for (; tree[ti].type != fotype && tree[ti].prev >= 0; ti = tree[ti].prev){}
 			if (tree[ti].type != fotype)
 			{
-				if (treesize == MAX_TREE_SIZE) throwError( BufferTooSmall);
-				if (stksize == MAX_SEARCH_SIZE) throwError( BufferTooSmall);
+				if (treesize == MAX_TREE_SIZE) throwError( BufferTooSmall, 0);
+				if (stksize == MAX_SEARCH_SIZE) throwError( BufferTooSmall, 0);
 				tree[ treesize].prev = fotreeidx;
 				tree[ treesize].type = fotype;
 				tree[ treesize].constructor = follow[ fi].constructor;
@@ -108,12 +152,31 @@ static bool shortestPathSearch( int32_t desttype, int32_t* fromtype, size_t from
 				++treesize;
 				++stksize;
 
-				if (fotype == desttype)
+				if (result->fromindex >= 0 && result->weight < foweight + 2E-24)
 				{
-
+					break;
+				}
+				if (match( context, fotype))
+				{
+					if (result->fromindex >= 0) {
+						result->arsize_alt = fillResult( result->ar_alt, tree, treesize-1);
+						result->ar_alt[0].constructor = -1;
+						break;
+					} else {
+						result->arsize = fillResult( result->ar, tree, treesize-1);
+						result->fromindex = result->ar_alt[0].constructor;
+						result->ar_alt[0].constructor = -1;
+						result->weight = foweight;
+					}
 				}
 			}
 		}
 	}
+	return (result->fromindex >= 0);
+}
+
+int main( int argc, char const* argv[])
+{
+	return 0;
 }
 
